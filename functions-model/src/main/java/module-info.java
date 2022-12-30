9edf367c389ec4fa5c8bdd0e356907db0d855a05
Clement Levallois <1244100+seinecle@users.noreply.github.com>
2022-12-30 18:30:35 +0100

    changed name of model folder to functions-model

---
+++ functions-model/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module net.clementlevallois.functions.model {
+   
+    requires jakarta.json;
+    requires jakarta.json.bind;
+    
+    exports net.clementlevallois.functions.model;
+    exports net.clementlevallois.functions.model.bibd;
+          
+}
